e8f37b3725c4cf16ec680ef6c8933022914c5f75
hongwenjun <hongwenjun@gmail.com>
2020-11-08 20:18:24 +0800

    ResEdit_Test

---
+++ 001_ResEdit_Test/resource.h
@@ -0,0 +1,11 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define DLG_MAIN                                100
+#define IDB_BITMAP1                             101
+#define IDB_BITMAP2                             102
+#define IDB_BITMAP3                             103
+#define IDB_BITMAP4                             104
+#define IDB_BITMAP5                             105
+#define IDI_ICON1                               106
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# cbstudy
-CodeBlocks视频教程学习代码示例
+# CodeBlocks视频教程学习代码示例
+
+蘭雅sRGB 龙芯2F服务器和博客 | [https://262235.xyz](https://262235.xyz)
+
+### Codeblocks 编程学习视频教程
+- B站频道: [https://space.bilibili.com/320303690/](https://space.bilibili.com/320303690/channel/detail?cid=156416)
+
+- YouTube频道: [https://www.youtube.com/sRGB18](https://www.youtube.com/watch?v=2FETiwhLEBQ&list=PLPidIcmhqWuTMftNgToJxQ5ssT4Ed3LgS)
+
+### 源码
+- 001_ResEdit_Test
